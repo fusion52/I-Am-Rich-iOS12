256f61832a1def5322faa29b6a8b94df76fc3ba4
Roberto Hernandez <rh@fusion52.com>
2019-01-31 20:46:12 -0800

    Initial commit of iOS 12 and Swift application

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Roberto Hernandez on 1/31/19.
+//  Copyright © 2019 Roberto Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
